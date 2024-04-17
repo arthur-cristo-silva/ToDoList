e920ecf2fc506b60e135647973c83b0ace31d5bf
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-04-16 23:53:39 -0300

    TodoService.java : Services created

---
+++ src/main/java/com/todo/list/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.todo.list.service;
+
+import com.todo.list.entity.Todo;
+import com.todo.list.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "prioridade")
+                .and(Sort.by(Sort.Direction.ASC, "name"));
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
